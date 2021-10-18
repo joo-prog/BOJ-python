fbc257e3d626084cf6a7fcdb70eee5708b2cea84
joo-prog <1114nhj@naver.com>
2021-10-18 13:01:35 +0900

    boj 10845, 10866

---
+++ code/10866.py
@@ -0,0 +1,40 @@
+import sys
+input=sys.stdin.readline
+
+n=int(input())
+
+deque=[]
+
+for i in range(n):
+    order=input().strip().split()
+    if order[0]=='push_front':
+        deque.insert(0,order[1])
+    elif order[0]=='push_back':
+        deque.append(order[1])
+    elif order[0]=='pop_front':
+        if len(deque)==0:
+            print(-1)
+        else:
+            print(deque.pop(0))
+    elif order[0]=='pop_back':
+        if len(deque)==0:
+            print(-1)
+        else:
+            print(deque.pop())
+    elif order[0]=='size':
+        print(len(deque))
+    elif order[0]=='empty':
+        if len(deque)==0:
+            print(1)
+        else:
+            print(0)
+    elif order[0]=='front':
+        if len(deque)==0:
+            print(-1)
+        else:
+            print(deque[0])
+    else:
+        if len(deque)==0:
+            print(-1)
+        else:
+            print(deque[-1])
