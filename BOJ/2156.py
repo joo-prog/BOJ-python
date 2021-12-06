f552bf1778ad2f3a17fc58a03392625ddd129bd3
joo-prog <1114nhj@naver.com>
2021-12-06 14:48:39 +0900

    edit boj 2156

--- BOJ/2156.py
+++ BOJ/2156.py
@@ -14,8 +14,7 @@ if n>=2:
     if n>=3:
         dp_wine[1]=wine[0]+wine[1]
         dp_wine[2]=max(dp_wine[1], wine[2]+wine[0], wine[1]+wine[2])
-        if n>=4:
-            for i in range(3, n):
-                dp_wine[i]=max(wine[i]+wine[i-1]+dp_wine[i-3], wine[i]+dp_wine[i-2], dp_wine[i-1])
+        for i in range(3, n):
+            dp_wine[i]=max(wine[i]+wine[i-1]+dp_wine[i-3], wine[i]+dp_wine[i-2], dp_wine[i-1])
 
 print(dp_wine[n-1])
