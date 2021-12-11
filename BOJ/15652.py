aaa288421ba712785807a5906a25fb3137b40068
joo-prog <1114nhj@naver.com>
2021-12-12 01:43:07 +0900

    edit boj 15652, 15654

--- BOJ/15652.py
+++ BOJ/15652.py
@@ -1,7 +1,7 @@
 from itertools import combinations_with_replacement
 
 n, m=map(int, input().split())
-li=list(map(int, input().split()))
+li=[i for i in range(1, n+1)]
 li.sort()
 for i in combinations_with_replacement(li, m):
     for j in i:
