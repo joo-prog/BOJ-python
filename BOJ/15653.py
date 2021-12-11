acd7648dea0bd446cf7518e7357db13781ffccc1
joo-prog <1114nhj@naver.com>
2021-12-12 01:44:12 +0900

    del boj 15653

--- BOJ/15653.py
+++
@@ -1,9 +0,0 @@
-from itertools import permutations
-
-n, m=map(int, input().split())
-li=list(map(int, input().split()))
-li.sort()
-for i in permutations(li, m):
-    for j in i:
-        print(j, end=' ')
-    print()
